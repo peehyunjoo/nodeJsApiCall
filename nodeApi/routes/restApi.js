5084df27a0339657a4894cf6d33af996eb601765
peehyunjoo <peehyunjoo@gmail.com>
2019-01-09 16:09:00 +0900

    api call response

--- nodeApi/routes/restApi.js
+++ nodeApi/routes/restApi.js
@@ -17,9 +17,7 @@ exports.post = function (req, res,next) {
     var port ="";
 
     var host = host.split('//');
-    console.log(host);
-    console.log(path);
-
+    console.log(method);
     if(host[0] == "http:"){
       var port ="80";
     }else{
@@ -29,7 +27,8 @@ exports.post = function (req, res,next) {
     var options = {
         hostname: host[1],
         path: path,
-        port:port
+        port:port,
+        method:method
       };
 
     function handleResponse(response) {
@@ -39,22 +38,21 @@ exports.post = function (req, res,next) {
       });
       response.on('end', function () {
         //console.log("Response Status:", response.statusCode);
-        //console.log("Response Headers:", response.headers);
-        //var serverData = prettyHtml(serverData, serverData.dimensions);
-        console.log(serverData);
+        //console.log(response.headers['content-type']);
+        //console.log(response.headers['date']);
+       console.log(serverData);
         res.send({statusCode:response.statusCode, headers:response.headers, resdata:serverData});
       });
     }
+
     if(port === "443"){
       https.request(options, function(response){
-        handleResponse(response);
+          handleResponse(response);
       }).end();
     }else if(port ==="80"){
       http.request(options, function(response){
         handleResponse(response);
       }).end();
     }
-
-
 
 }
